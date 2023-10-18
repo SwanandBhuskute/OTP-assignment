7e34dbd5c3885d6c3596e68c3a74a0fa57531dc8
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-10-18 18:36:43 +0530

    Version 1 commited

---
+++ codesOtpSms.py
@@ -0,0 +1,7 @@
+account_sid = 'AC1a01a4fd1cc7cdbb358e19fe12b9ce93'
+auth_token = '1fbcb17dfe649c3d4476b8d0330e07dc'
+
+twilio_num = '+15735944610'
+target = '+918625847883'
+
+# target = ['+918625847883', '+919075382704']
